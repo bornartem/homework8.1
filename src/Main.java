806ab4859141763e51617c6cb1c73780bd7883e2
bornartem <born.artem@gmail.com>
2023-08-02 22:33:47 +0300

    2commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
         public class Main {
             public static void main(String[] args) {
-                int[] numbers = new int[3]; // Задача 1
+                int[] numbers = new int[3]; // Задача 1.1
                 numbers[0] = 1;
                 numbers[1] = 2;
                 numbers[2] = 3;
